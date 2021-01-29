dac1c11efe76a9aaae29dfedadd661f3308537f0
Elena Lushnikova <elushnikova.256@gmail.com>
2021-01-29 23:05:25 +0300

    test(WeatherWidget): Cover code

---
+++ tests/unit/WeatherWidget.spec.ts
@@ -0,0 +1,14 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import WeatherWidget from "@/WeatherWidget.vue";
+import CityCard from "@/components/CityCard.vue";
+
+describe("WeatherWidget.vue", () => {
+  it("renders CityCard component", () => {
+    const localVue = createLocalVue();
+    const wrapper = shallowMount(WeatherWidget, { localVue });
+    const cityCard = wrapper.find(CityCard);
+    
+    expect(cityCard.exists()).toBe(true);
+    wrapper.destroy();
+  });
+});
