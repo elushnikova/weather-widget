019e8711ee15be5b1ac856c2c574a1cf13d4a487
Elena Lushnikova <elushnikova.256@gmail.com>
2021-01-29 23:04:52 +0300

    test(CityCard): Use localVue; destroy wrapper

--- tests/unit/CityCard.spec.ts
+++ tests/unit/CityCard.spec.ts
@@ -1,12 +1,16 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import CityCard from "@/components/CityCard.vue";
 
 describe("CityCard.vue", () => {
   it("renders props.city when passed", () => {
     const city = "Moscow";
+    const localVue = createLocalVue();
     const wrapper = shallowMount(CityCard, {
+      localVue,
       propsData: { city }
     });
+    
     expect(wrapper.text()).toMatch(city);
+    wrapper.destroy();
   });
 });
