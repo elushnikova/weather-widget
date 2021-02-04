ddd2ed9bc1a85f7689ac54b2a007cbc92dddae98
Elena Lushnikova <elushnikova.256@gmail.com>
2021-02-04 20:10:10 +0300

    feat: Add AppList and AppListItem

---
+++ src/components/AppList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="ww-list" :class="{ 'ww-list--row': row, 'ww-list--wrap': wrap }">
+    <slot />
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppList",
+
+  props: {
+    row: { type: Boolean, default: false },
+    wrap: { type: Boolean, default: false }
+  }
+});
+</script>
+
+<style lang="scss">
+.ww-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.ww-list--row {
+  display: flex;
+}
+
+.ww-list--wrap {
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/AppListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="ww-list__item" :class="{ 'ww-list__item--half': half }">
+    <slot />
+  </li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppListItem",
+
+  props: {
+    half: { type: Boolean, default: false }
+  }
+});
+</script>
+
+<style lang="scss">
+.ww-list__item {
+  flex-shrink: 0;
+  display: flex;
+  padding-top: 4px;
+  padding-bottom: 4px;
+}
+
+.ww-list__item--half {
+  flex-basis: 50%;
+}
+</style>
