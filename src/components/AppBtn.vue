61f4613b0b946dc5d0fac921d1e94804f39552cd
Elena Lushnikova <elushnikova.256@gmail.com>
2021-02-06 09:33:14 +0300

    feat: Add AppView and AppBtn

---
+++ src/components/AppBtn.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    class="ww-btn"
+    :class="{ 'ww-btn--view': view }"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppBtn",
+
+  props: {
+    view: { type: Boolean, default: false }
+  }
+});
+</script>
+
+<style lang="scss">
+.ww-btn {
+  box-sizing: border-box;
+  padding: 4px;
+  border-radius: 50%;
+  border: 0;
+  background-color: transparent;
+  transition: background-color 300ms;
+
+  &:focus {
+    background-color: lightgrey;
+    outline: none;
+  }
+}
+
+.ww-btn--view {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+}
+</style>
---
+++ src/components/AppView.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition name="fade">
+    <div class="ww-view" v-if="show" :key="name">
+      <slot />
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppView",
+
+  props: {
+    name: { type: String, required: true },
+    show: { type: Boolean, required: true }
+  }
+});
+</script>
+
+<style lang="scss">
+.ww-view {
+  position: absolute;
+  box-sizing: border-box;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity, transform;
+  transition-duration: 500ms;
+  transition-timing-function: ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+</style>
