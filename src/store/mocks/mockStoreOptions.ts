47e38e4d2571b2bd9c0cbe1acd831a794fe03ba5
Elena Lushnikova <elushnikova.256@gmail.com>
2021-02-09 14:47:43 +0300

    test: Mock all Vuex actions

--- src/store/mocks/mockStoreOptions.ts
+++ src/store/mocks/mockStoreOptions.ts
@@ -1,5 +1,6 @@
 import weatherItem from "@/classes/mocks/weatherItem";
 import Weather from "@/classes/Weather";
+import actions from "@/store/actions";
 import getters from "@/store/getters";
 import mutations from "@/store/mutations";
 import StateInterface from "@/types/interfaces/StateInterface";
@@ -10,10 +11,11 @@ const mockState: StateInterface = {
   weatherList: [mockWeather],
   locationList: [mockWeather.location],
 };
-const mockActions = {
-  searchLocation(): void {
-    return;
-  },
+const mockActions: typeof actions = {
+  remove: jest.fn(),
+  setList: jest.fn(),
+  searchLocation: jest.fn().mockResolvedValue(mockWeather.location),
+  fetchWeather: jest.fn().mockResolvedValue(mockWeather),
 };
 
 const mockStoreOptions = {
