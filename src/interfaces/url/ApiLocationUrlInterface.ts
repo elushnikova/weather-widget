1572b2ca441c1c3bf87e34bd140b66636cc21f11
Elena Lushnikova <elushnikova.256@gmail.com>
2021-02-10 11:24:50 +0300

    chore: Remove unused interface for ApiLocationUrl

--- src/interfaces/url/ApiLocationUrlInterface.ts
+++
@@ -1,7 +0,0 @@
-import ApiUrlInterface from "@/interfaces/url/ApiUrlInterface";
-
-interface ApiLocationUrlInterface extends ApiUrlInterface {
-  location: string;
-}
-
-export default ApiLocationUrlInterface;
